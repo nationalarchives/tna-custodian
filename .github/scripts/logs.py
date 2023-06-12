d18e4223e5464f69b5964179c8cc1afe39eeba2c
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2023-06-12 14:37:54 +0100

    Add some logging statements

--- .github/scripts/logs.py
+++ .github/scripts/logs.py
@@ -13,14 +13,15 @@ with open(sys.argv[1]) as file:
     log_event = [{'timestamp': timestamp, 'message': file.read()}]
 
 client.create_log_stream(logGroupName=log_group_name, logStreamName=log_stream_name)
+print("log stream created")
 response = client.put_log_events(logGroupName=log_group_name,
                                  logStreamName=log_stream_name,
                                  logEvents=log_event)
-
+print("log event added")
 base_url = "https://eu-west-2.console.aws.amazon.com/cloudwatch/home"
 encoded_stream_name = quote_plus(quote_plus(log_stream_name))
 fragment = f"logsV2:log-groups/log-group/{log_group_name}/log-events/{encoded_stream_name}"
 url = f"{base_url}?region=eu-west-2#{fragment}"
-
+print("url created")
 with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
     print(f"log-url={url}", file=fh)
