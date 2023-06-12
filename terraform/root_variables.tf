9c28ac6e47ade7d08d278bc20d1135fa3cca173f
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2023-06-12 13:09:17 +0100

    Parameterise the backend

--- terraform/root_variables.tf
+++ terraform/root_variables.tf
@@ -1,12 +1,10 @@
 variable "project" {
   description = "abbreviation for the project, e.g. tdr, forms the first part of resource names"
-  default     = "tdr"
 }
 
 variable "account_number" {
   description = "The AWS account number where the TDR environment is hosted"
   type        = string
-  default     = ""
 }
 
 variable "assume_role" {
