7a740cbeab2eb2e1cb36ee66be046ca3bb9a70ed
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2023-06-14 13:10:04 +0100

    Remove count

--- terraform/root_data.tf
+++ terraform/root_data.tf
@@ -1,14 +1,11 @@
 data "aws_ssm_parameter" "intg_account_number" {
-  count = local.environment == "mgmt" ? 1 : 0
   name  = local.account_parameter_names[var.project]["intg"]
 }
 
 data "aws_ssm_parameter" "staging_account_number" {
-  count = local.environment == "mgmt" ? 1 : 0
   name  = local.account_parameter_names[var.project]["staging"]
 }
 
 data "aws_ssm_parameter" "prod_account_number" {
-  count = local.environment == "mgmt" ? 1 : 0
   name  = local.account_parameter_names[var.project]["prod"]
 }
