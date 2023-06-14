bbf0540b116569a0f47629cc3eddc9a89a51280d
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2023-06-14 13:11:32 +0100

    Remove instance key

--- terraform/root_main.tf
+++ terraform/root_main.tf
@@ -15,9 +15,9 @@ module "encryption_key" {
   common_tags = local.common_tags
   function    = "sqs"
   key_policy  = "multi_account"
-  intg_account_number = data.aws_ssm_parameter.intg_account_number[0].value
-  staging_account_number = data.aws_ssm_parameter.staging_account_number[0].value
-  prod_account_number = data.aws_ssm_parameter.prod_account_number[0].value
+  intg_account_number = data.aws_ssm_parameter.intg_account_number.value
+  staging_account_number = data.aws_ssm_parameter.staging_account_number.value
+  prod_account_number = data.aws_ssm_parameter.prod_account_number.value
 }
 
 module "sqs" {
@@ -25,7 +25,7 @@ module "sqs" {
   project           = var.project
   common_tags       = local.common_tags
   kms_master_key_id = module.encryption_key.kms_key_arn
-  intg_account_number = data.aws_ssm_parameter.intg_account_number[0].value
-  staging_account_number = data.aws_ssm_parameter.staging_account_number[0].value
-  prod_account_number = data.aws_ssm_parameter.prod_account_number[0].value
+  intg_account_number = data.aws_ssm_parameter.intg_account_number.value
+  staging_account_number = data.aws_ssm_parameter.staging_account_number.value
+  prod_account_number = data.aws_ssm_parameter.prod_account_number.value
 }
