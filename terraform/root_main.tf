94efa9fea9434a856ea724484684e6f2e7ae46fd
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2023-06-21 11:59:58 +0100

    Ran terraform fmt

--- terraform/root_main.tf
+++ terraform/root_main.tf
@@ -1,5 +1,5 @@
 module "terraform_config" {
-  source = "./da-terraform-configurations/"
+  source  = "./da-terraform-configurations/"
   project = var.project
 }
 
@@ -10,23 +10,23 @@ module "iam" {
 }
 
 module "encryption_key" {
-  source      = "./modules/kms"
-  project     = var.project
-  environment = local.environment
-  common_tags = local.common_tags
-  function    = "sqs"
-  key_policy  = "multi_account"
-  intg_account_number = module.terraform_config.account_numbers["intg"]
+  source                 = "./modules/kms"
+  project                = var.project
+  environment            = local.environment
+  common_tags            = local.common_tags
+  function               = "sqs"
+  key_policy             = "multi_account"
+  intg_account_number    = module.terraform_config.account_numbers["intg"]
   staging_account_number = module.terraform_config.account_numbers["staging"]
-  prod_account_number = module.terraform_config.account_numbers["prod"]
+  prod_account_number    = module.terraform_config.account_numbers["prod"]
 }
 
 module "sqs" {
-  source            = "./modules/sqs"
-  project           = var.project
-  common_tags       = local.common_tags
-  kms_master_key_id = module.encryption_key.kms_key_arn
-  intg_account_number = module.terraform_config.account_numbers["intg"]
+  source                 = "./modules/sqs"
+  project                = var.project
+  common_tags            = local.common_tags
+  kms_master_key_id      = module.encryption_key.kms_key_arn
+  intg_account_number    = module.terraform_config.account_numbers["intg"]
   staging_account_number = module.terraform_config.account_numbers["staging"]
-  prod_account_number = module.terraform_config.account_numbers["prod"]
+  prod_account_number    = module.terraform_config.account_numbers["prod"]
 }
