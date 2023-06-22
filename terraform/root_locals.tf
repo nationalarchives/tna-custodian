536752d3df16d4e6a53a708e3727ac034d371827
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2023-06-22 10:39:11 +0100

    Get account role from config

--- terraform/root_locals.tf
+++ terraform/root_locals.tf
@@ -1,6 +1,6 @@
 locals {
   environment = lower(terraform.workspace)
-  assume_role = var.project == "dr2" ? "arn:aws:iam::${var.account_number}:role/${title(local.environment)}TerraformRole" : local.environment == "mgmt" || local.environment == "sbox" || local.environment == "ddri" ? "arn:aws:iam::${var.account_number}:role/IAM_Admin_Role" : "arn:aws:iam::${var.account_number}:role/TDRTerraformRole${title(local.environment)}"
+  assume_role = module.terraform_config.terraform_config[local.environment]["terraform_account_role"]
   common_tags = tomap(
     {
       "Environment" = local.environment,
